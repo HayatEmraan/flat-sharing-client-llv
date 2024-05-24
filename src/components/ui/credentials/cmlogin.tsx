320e2a36ae02cc74379af230f1a9c732fde49d77
hayatemraan <emraanhasanhayat@gmail.com>
2024-05-24 12:57:17 +0600

    feat(credentials): signin & register added

--- src/components/ui/credentials/cmlogin.tsx
+++ src/components/ui/credentials/cmlogin.tsx
@@ -1,35 +1,80 @@
 "use client";
 
+import { LoginAction } from "@/actions/alogin/loginaction";
 import FsForm from "@/components/shared/form/fsform";
-import { useFormContext } from "react-hook-form";
+import { TLoginUser } from "@/interface";
+import { useRouter } from "next/navigation";
+import { KeyboardEvent, useState } from "react";
+import { FieldValues, useFormContext } from "react-hook-form";
+import { Report } from "notiflix/build/notiflix-report-aio";
 
 const CmLogin = () => {
-  const onSubmit = (data: any) => console.log(data);
+  const [submitLoading, setSubmitLoading] = useState<boolean>(false);
+  const router = useRouter();
+  const onSubmit = async (data: FieldValues) => {
+    setSubmitLoading(true);
+
+    const login = await LoginAction(data as TLoginUser);
+    if (login?.statusCode === 200) {
+      setSubmitLoading(false);
+      Report.success(
+        "{Sign In} Success",
+        '"Do not try to become a person of success but try to become a person of value." <br/><br/>- Albert Einstein',
+        "{ Explore }",
+        () => {
+          router.push("/");
+        }
+      );
+    } else {
+      setSubmitLoading(false);
+
+      Report.failure(
+        "{Sign In} Failure",
+        '"Failure is simply the opportunity to begin again, this time more intelligently." <br/><br/>- Henry Ford',
+        "{ Try, again }",
+        () => {}
+      );
+    }
+  };
 
   return (
     <FsForm onSubmit={onSubmit}>
-      <CmLoginValues />
+      <CmLoginValues loading={submitLoading} />
     </FsForm>
   );
 };
 
-const CmLoginValues = () => {
+const CmLoginValues = ({ loading }: { loading: boolean }) => {
   const { register } = useFormContext();
+
+  function noSpaces(event: KeyboardEvent<HTMLInputElement>) {
+    if (event.key === " ") {
+      event.preventDefault();
+    }
+  }
+
   return (
     <>
       <input
         type="text"
         {...register("emailOrUsername")}
         placeholder="Email / Username"
+        onKeyDown={(event) => noSpaces(event)}
         required
       />
       <input
         type="password"
         {...register("password")}
+        onKeyDown={(event) => noSpaces(event)}
         placeholder="Password"
         required
       />
-      <button type="submit">Sign In</button>
+      <button
+        disabled={loading}
+        className={`mb-8  ${loading && "!cursor-not-allowed"}`}
+        type="submit">
+        {loading ? "Loading." : "{ Sign In }"}
+      </button>
     </>
   );
 };
--- src/components/ui/credentials/cmregister.tsx
+++ src/components/ui/credentials/cmregister.tsx
@@ -1,30 +1,67 @@
 "use client";
 
+import { mailAction } from "@/actions/aregister/mailaction";
+import { RegisterAction } from "@/actions/aregister/registeraction";
 import { usernameAction } from "@/actions/aregister/usernameaction";
 import FsForm from "@/components/shared/form/fsform";
-import { KeyboardEvent, useState } from "react";
-import { useFormContext } from "react-hook-form";
+import { TRegisterUser } from "@/interface";
+import { ChangeEvent, KeyboardEvent, useState } from "react";
+import { FieldValues, useFormContext } from "react-hook-form";
+import { Confirm } from "notiflix/build/notiflix-confirm-aio";
+import { useRouter } from "next/navigation";
 
 const CmRegister = () => {
-  const onSubmit = (data: any) => console.log(data);
+  const [submitLoading, setSubmitLoading] = useState<boolean>(false);
+  const router = useRouter();
+  const onSubmit = async (data: FieldValues) => {
+    setSubmitLoading(true);
+    delete data.cPassword;
+    const register = await RegisterAction(data as TRegisterUser);
+    if (register?.statusCode === 200) {
+      setSubmitLoading(false);
+      Confirm.show(
+        "Congratulations 🎉",
+        "Your account successfully created, please check your mail.",
+        "{ Sign In }",
+        "Stay here",
+        () => {
+          router.push("/auth/login");
+        },
+        () => {}
+      );
+    } else {
+      setSubmitLoading(false);
+      Confirm.show(
+        "Oops, Something went wrong",
+        "Error while opening an Account, please try again later!",
+        "{ Back }",
+        "Stay here",
+        () => {
+          router.push("/");
+        },
+        () => {}
+      );
+    }
+  };
 
   return (
     <FsForm onSubmit={onSubmit}>
-      <CmRegisterValues />
+      <CmRegisterValues loading={submitLoading} />
     </FsForm>
   );
 };
 
-const CmRegisterValues = () => {
+const CmRegisterValues = ({ loading }: { loading: boolean }) => {
   const { register, watch } = useFormContext();
 
-  const [username, email] = watch(["username", "email"]);
+  const [password] = watch(["password"]);
 
   const [mailError, setMailError] = useState<string>();
   const [usernameError, setUsernameError] = useState<string>();
 
-  const [mailLoading, setMailLoading] = useState(false);
-  const [usernameLoading, setUsernameLoading] = useState(false);
+  const [matchPassword, setMatchPassword] = useState<string>();
+
+  const [error, setError] = useState<boolean>(false);
 
   function noSpaces(event: KeyboardEvent<HTMLInputElement>) {
     if (event.key === " ") {
@@ -33,36 +70,53 @@ const CmRegisterValues = () => {
   }
 
   const checkingUserName = async (str: string) => {
-    setUsernameLoading(true);
+    setUsernameError(`available checking...`);
+    if (str.length === 0) {
+      setError(true);
+      return setUsernameError("No spaces allowed");
+    }
+
     const usernameExistOrNot = await usernameAction(str);
-    if (usernameExistOrNot?.data?.status === 200) {
-      setUsernameLoading(false);
-      setUsernameError(`Congrats, ${username} is available`);
-    } else if (str.length === 0) {
-      setUsernameError("No spaces allowed");
+
+    if (usernameExistOrNot?.statusCode === 200) {
+      setError(false);
+      setUsernameError(`congrats, available`);
     } else {
-      setUsernameError(`${username} isn't available`);
+      setUsernameError(`oops! not available`);
+      setError(true);
     }
   };
 
   const checkingMail = async (str: string) => {
-    setMailLoading(true);
-    const usernameExistOrNot = await usernameAction(str);
-    if (usernameExistOrNot?.data?.status === 200) {
-      setUsernameLoading(false);
+    const mailExistOrNot = await mailAction(str);
+    if (mailExistOrNot?.statusCode === 200) {
+      setError(false);
       setMailError("");
-    } else {
-      setMailError(`${email} already exist`);
+    } else if (!mailExistOrNot?.success) {
+      setError(true);
+      setMailError(`oops! already exist`);
     }
   };
 
-  if (username) {
-    // checkingUserName(username);
-    console.log("something");
-  }
-  if (email) {
-    checkingMail(email);
-  }
+  const onChangeUsername = (e: ChangeEvent<HTMLInputElement>) => {
+    checkingUserName(e.target.value);
+  };
+  const onChangeMail = (e: ChangeEvent<HTMLInputElement>) => {
+    checkingMail(e.target.value);
+  };
+
+  const onChangeConfirmPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    const cPassword = e.target.value;
+    if (cPassword && password) {
+      if (cPassword === password) {
+        setError(false);
+        setMatchPassword("");
+      } else {
+        setError(true);
+        setMatchPassword("password not match");
+      }
+    }
+  };
 
   return (
     <>
@@ -74,32 +128,52 @@ const CmRegisterValues = () => {
           required
           pattern="^\S*$"
           onKeyDown={(event) => noSpaces(event)}
+          onChange={(e) => onChangeUsername(e)}
         />
         <span className="tooltip-text">
           {usernameError ? usernameError : "No spaces allowed"}
         </span>
       </div>
-      <input
-        type="email"
-        {...register("email")}
-        onKeyDown={(event) => noSpaces(event)}
-        placeholder="Email"
-        required
-      />
+
+      <div className="tooltip">
+        <input
+          type="email"
+          {...register("email")}
+          onKeyDown={(event) => noSpaces(event)}
+          placeholder="Email"
+          required
+          onChange={(e) => onChangeMail(e)}
+        />
+        {mailError && <span className="tooltip-text">{mailError}</span>}
+      </div>
       <input
         type="password"
         {...register("password")}
+        onKeyDown={(event) => noSpaces(event)}
         placeholder="Password"
         required
       />
-      <input
-        type="password"
-        {...register("cPassword")}
-        placeholder="Confirm Password"
-        required
-      />
-      <button className="mb-8" type="submit">
-        Sign Up
+
+      <div className="tooltip">
+        <input
+          type="password"
+          {...register("cPassword")}
+          placeholder="Confirm Password"
+          onKeyDown={(event) => noSpaces(event)}
+          required
+          className={matchPassword && "!outline !outline-red-400 !outline-1"}
+          disabled={!password}
+          onChange={(e) => onChangeConfirmPassword(e)}
+        />
+        {mailError && <span className="tooltip-text">{mailError}</span>}
+        {!password && <span className="tooltip-text">type, password 1st</span>}
+        {matchPassword && <span className="tooltip-text">{matchPassword}</span>}
+      </div>
+      <button
+        disabled={loading || error}
+        className={`mb-8  ${loading || (error && "!cursor-not-allowed")}`}
+        type="submit">
+        {loading ? "Loading." : "{ Sign Up }"}
       </button>
     </>
   );
